09759265b1eba6bb5e00c6bf1d57902a3914e346
vriznet <vriz@vriz.net>
2021-06-01 23:15:36 +0900

    6.6 seeHashtag

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -5,4 +5,22 @@ export default {
     user: ({ id }) => client.photo.findUnique({ where: { id } }).user(),
     hashtags: ({ id }) => client.photo.findUnique({ where: { id } }).hashtags(),
   },
+  Hashtag: {
+    photos: ({ id }, { page }) => {
+      return client.hashtag.findUnique({ where: { id } }).photos({
+        take: 5,
+        skip: (page - 1) * 5,
+      });
+    },
+    totalPhotos: ({ id }) =>
+      client.photo.count({
+        where: {
+          hashtags: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+  },
 };
